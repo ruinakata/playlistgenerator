f9d67a1a70e84520e596ce9f4d8fd2ce2b021a58
Rui Nakata <rnakata@colgate.edu>
2014-08-04 14:36:41 -0500

    wrote seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,68 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+simp1 = JSON.parse(File.read('simplifiedtracks1.json'))
+simp2 = JSON.parse(File.read('simplifiedtracks2.json'))
+simp3 = JSON.parse(File.read('simplifiedtracks3.json'))
+simp4 = JSON.parse(File.read('simplifiedtracks4.json'))
+
+arrayofurls1 = simp1.keys
+arrayofurls2 = simp2.keys
+arrayofurls3 = simp3.keys
+arrayofurls4 = simp4.keys
+
+index = 0
+playlistid = index + 1 #1
+#loop thru playlists
+while index < arrayofurls1.length
+  url = arrayofurls1[index]
+  arrayoftracks = simp1[url]
+  #loop thru tracks in playlist
+  arrayoftracks.each do |track|
+    track[:playlist_id] = playlistid
+    Track.create(track)
+  end
+  index += 1
+  playlistid += 1
+end
+
+
+index = 0
+playlistid = arrayofurls1.length + 1 #701
+while index < arrayofurls2.length
+  url = arrayofurls2[index]
+  arrayoftracks = simp2[url]
+  arrayoftracks.each do |track|
+    track[:playlist_id] = playlistid
+    Track.create(track)
+  end
+  index += 1
+  playlistid += 1
+end
+
+
+index = 0
+playlistid = arrayofurls1.length + arrayofurls2.length + 1 
+while index < arrayofurls3.length
+  url = arrayofurls3[index]
+  arrayoftracks = simp3[url]
+  arrayoftracks.each do |track|
+    track[:playlist_id] = playlistid
+    Track.create(track)
+  end
+  index += 1
+  playlistid += 1
+end
+
+
+index = 0
+playlistid = arrayofurls1.length + arrayofurls2.length + arrayofurls3.length + 1 
+while index < arrayofurls4.length
+  url = arrayofurls4[index]
+  arrayoftracks = simp4[url]
+  arrayoftracks.each do |track|
+    track[:playlist_id] = playlistid
+    Track.create(track)
+  end
+  index += 1
+  playlistid += 1
+end
